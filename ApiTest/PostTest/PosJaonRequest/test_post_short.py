dddf4509ba27c0cd9dceb94f7b39f4ad0e1836ee
Benny <bennyscash1@gmail.com>
2026-02-16 10:10:36 +0200

    init main

--- ApiTest/PostTest/PosJaonRequest/test_post_short.py
+++ ApiTest/PostTest/PosJaonRequest/test_post_short.py
@@ -1,13 +1,16 @@
-import pytest
-import requests
 import sys
-import os
+from pathlib import Path
 
+# Ensure project root is on path when running/debugging this file directly
+ROOT = Path(__file__).resolve().parent.parent.parent.parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+
+import pytest
 from ApiTest.CommonApiService.api_services import ApiServices
-from Infra.BaseData.CommonData import TestLevel
-from Infra.BaseData.GetData import loaded_data, VarData
-from ApiTest.HttpServices.http_requests import HttpService, HttpServiceOptions
-from ApiTest.PostTest.PosJaonRequest.pos_dto import PostPayloadDTO, PostResponseDTO
+from Infra.BaseData.CommonData import RetryFauilureTest, TestLevel
+
+@RetryFauilureTest()
 @TestLevel.level0
 def test_post_service():
     api_services = ApiServices()
--- Infra/BaseData/CommonData.py
+++ Infra/BaseData/CommonData.py
@@ -20,3 +20,21 @@ class EnumData(Enum):
 class TestLevel:
     level0 = pytest.mark.level0
     level1 = pytest.mark.level1
+
+class RetryFauilureTest:
+    def __init__(self, max_retries=3):
+        self.max_retries = max_retries
+    
+    def __call__(self, func):
+        from functools import wraps
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(self.max_retries):
+                print(f"Attempt tesst failure {attempt + 1}/{self.max_retries}")
+                try:
+                    return func(*args, **kwargs)
+                except AssertionError:
+                    if attempt == self.max_retries - 1:
+                      #  print(f"Test failed after {self.max_retries} attempts")
+                        raise
+        return wrapper
